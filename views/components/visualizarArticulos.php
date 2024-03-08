0fcf084a3f9f08633e64a9d92c3b2adef9104795
CarlosFerMed <harmonium_skies@hotmail.com>
2024-03-08 14:42:29 +0100

    Revisado problema que no mostraba los iconos al realizar busquedas concretas, v1.4.1

--- views/components/visualizarArticulos.php
+++ views/components/visualizarArticulos.php
@@ -5,27 +5,11 @@
     }
 
     include "models/publicaciones.php";   
-
-
-    // $url = "http://localhost:3000/";
-
-    // if (isset($_SESSION["usuario"])) 
-    //     $url = "sesion";
-    
-    // function sesionSioNo() {
-        // if ($_SESSION["usuario"])
-        //     echo "http://localhost:3000/sesion";
-        // else 
-        //     echo "http://localhost:3000/";
-    // }
-    // echo $url;
 ?>
 <!-- Este archivo debe ser una especie de Template para introducir en los archivos donde queramos
 que se muestren los artículos -->
 <main>
     <?php 
-        // $bool = isset($tipo);
-
         if ((isset($tipo)) && ($tipo !== "todas")) {
             $articulos = array();
             $publicaciones = new conexionPublicaciones();
@@ -35,7 +19,7 @@ que se muestren los artículos -->
             }
             if ($articulos != null) {
                 foreach($articulos as $articulo) {
-                    echo "<div class='item'> Título: <a class='enlace' href='/mostrarArticuloIndividual?id=" . $articulo->id . "'>" 
+                    echo "<div class='". $articulo->tipo ."'> Título: <a class='enlace' href='/mostrarArticuloIndividual?id=" . $articulo->id . "'>" 
                         . $articulo->titulo . " </a><br>Temática: " . $articulo->tipo . "<br>Fecha:  " . $articulo->fecha 
                         . "<br>Creador: " . $articulo->usuarioCreador . "</div><br><br>";
                 }
@@ -73,10 +57,3 @@ que se muestren los artículos -->
     <input type="text" value="<?= $redireccionamiento ?>" name="url" hidden>
     <input type="submit" value="Filtrar" class="boton"/>
 </form>
-
-
-<!-- <div style="display: flex; align-items: center; border: 2px solid black; width: 300px; height: 300px">
-    <img src="public/img/azura.jpg" alt="Foto Azura" style="height: 300px"/>
-
-    echo "<a href='/mostrarArticuloIndividual?id=" . $articulo->id . "'><div class='item'> título: " . $articulo->titulo . " <br> contenido: " . $articulo->contenido . " <br> fecha:  " . $articulo->fecha . "</div></a><br><br>";
-</div> -->
